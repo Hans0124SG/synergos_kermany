7faaa70d8697d500b472e7a7f4b1418d9916f058
root <wh1996fz@live.com>
2022-01-11 09:52:30 +0800

    last before moving

--- start_worker1.sh
+++ start_worker1.sh
@@ -1,4 +1,4 @@
-docker container run -v ~/oct-data/CellData/OCT/data1:/worker/data \
-    -v ~/oct-data/CellData/OCT/outputs_1:/worker/outputs \
-    --name worker_1 synergos_worker:v0.1.0
+docker container run -v /root/CellData/OCT/synergos_kermany/kermany/data1:/worker/data \
+    -v /root/CellData/OCT/synergos_kermany/kermany/outputs_1:/worker/outputs \
+    --name worker_1 synergos_worker:v0.1.0 \
     --logging_variant basic
--- start_worker2.sh
+++ start_worker2.sh
@@ -1,3 +1,4 @@
-docker container run -v ~/oct-data/CellData/OCT/data2:/worker/data \
-    -v ~/oct-data/CellData/OCT/outputs_2:/worker/outputs \
-    --name worker_2 synergos_worker:v0.1.0
+docker container run -v /root/CellData/OCT/synergos_kermany/kermany/data2:/worker/data \
+    -v /root/CellData/OCT/synergos_kermany/outputs_2:/worker/outputs \
+    --name worker_2 synergos_worker:v0.1.0 \
+    --logging_variant basic
